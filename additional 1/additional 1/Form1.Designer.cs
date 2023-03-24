6d77b4b2259716607e3026ea613cda50166d9eed
Maksym Kashchuk <39593404+maxkashchuk@users.noreply.github.com>
2023-03-24 21:16:21 +0200

    Add files via upload

---
+++ additional 1/additional 1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace additional_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.aValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.quntityValue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.matematychneSpodivannyaField = new System.Windows.Forms.TextBox();
+            this.serednioKvadratychneVidhylenniaField = new System.Windows.Forms.TextBox();
+            this.kilkistZnachenN = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(12, 12);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(454, 401);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(566, 385);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(130, 53);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Обрахувати";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.button_Click);
+            // 
+            // aValue
+            // 
+            this.aValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.aValue.Location = new System.Drawing.Point(498, 112);
+            this.aValue.Name = "aValue";
+            this.aValue.Size = new System.Drawing.Size(66, 38);
+            this.aValue.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(540, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Діапазон";
+            // 
+            // bValue
+            // 
+            this.bValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bValue.Location = new System.Drawing.Point(595, 112);
+            this.bValue.Name = "bValue";
+            this.bValue.Size = new System.Drawing.Size(66, 38);
+            this.bValue.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(518, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "а";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(619, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "b";
+            // 
+            // quntityValue
+            // 
+            this.quntityValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.quntityValue.Location = new System.Drawing.Point(707, 112);
+            this.quntityValue.Name = "quntityValue";
+            this.quntityValue.Size = new System.Drawing.Size(66, 38);
+            this.quntityValue.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(687, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Кількість";
+            // 
+            // matematychneSpodivannyaField
+            // 
+            this.matematychneSpodivannyaField.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.matematychneSpodivannyaField.Location = new System.Drawing.Point(498, 224);
+            this.matematychneSpodivannyaField.Name = "matematychneSpodivannyaField";
+            this.matematychneSpodivannyaField.Size = new System.Drawing.Size(112, 38);
+            this.matematychneSpodivannyaField.TabIndex = 9;
+            // 
+            // serednioKvadratychneVidhylenniaField
+            // 
+            this.serednioKvadratychneVidhylenniaField.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.serednioKvadratychneVidhylenniaField.Location = new System.Drawing.Point(651, 224);
+            this.serednioKvadratychneVidhylenniaField.Name = "serednioKvadratychneVidhylenniaField";
+            this.serednioKvadratychneVidhylenniaField.Size = new System.Drawing.Size(122, 38);
+            this.serednioKvadratychneVidhylenniaField.TabIndex = 10;
+            // 
+            // kilkistZnachenN
+            // 
+            this.kilkistZnachenN.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kilkistZnachenN.Location = new System.Drawing.Point(566, 316);
+            this.kilkistZnachenN.Name = "kilkistZnachenN";
+            this.kilkistZnachenN.Size = new System.Drawing.Size(123, 38);
+            this.kilkistZnachenN.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(631, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 32);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Середньоквадратичне\r\n          відхилення";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(585, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = " Кількість\r\nЗначень N";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(495, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 32);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Математичне\r\n  сподівання";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.kilkistZnachenN);
+            this.Controls.Add(this.serednioKvadratychneVidhylenniaField);
+            this.Controls.Add(this.matematychneSpodivannyaField);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.quntityValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aValue);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox aValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox bValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox quntityValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox matematychneSpodivannyaField;
+        private System.Windows.Forms.TextBox serednioKvadratychneVidhylenniaField;
+        private System.Windows.Forms.TextBox kilkistZnachenN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ additional 1/additional 1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace additional_1
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        List<int> X;
+
+        double Yi;
+
+        double matematychne_spodivanya;
+
+        double seredniokvadratychne_vidhylennia;
+
+        double kilkistN;
+
+        List<int> generateX()
+        {
+            for (int i = 0; i < Int32.Parse(quntityValue.Text); i++)
+            {
+                X.Add(rnd.Next(Int32.Parse(aValue.Text), Int32.Parse(bValue.Text)));
+            }
+            for(int i = 0; i < X.Count; i++)
+            {
+                Console.WriteLine("Value " + i + " equals " + X[i] + '\n');
+            }
+            return X;
+        }
+
+        double Calculate(ref List<int> X)
+        {
+            double sum = 0;
+            foreach(int i in X)
+            {
+                sum += i;
+            }
+            sum = sum - (kilkistN / 2);
+            return matematychne_spodivanya + (seredniokvadratychne_vidhylennia / Math.Sqrt(kilkistN / 12)) * sum;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            chart.Series.Clear();
+            chart.Titles.Add("Formula values");
+            chart.Series.Add("Values");
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            double value;
+            kilkistN = Double.Parse(kilkistZnachenN.Text);
+            matematychne_spodivanya = Double.Parse(matematychneSpodivannyaField.Text);
+            seredniokvadratychne_vidhylennia = Double.Parse(serednioKvadratychneVidhylenniaField.Text);
+            X = new List<int>();
+            X = generateX();
+            value = Calculate(ref X);
+            Console.WriteLine("\n\nResult is: " + value);
+            chart.Series["Values"].Points.Add(value);
+            chart.Update();
+        }
+    }
+}
